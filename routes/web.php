b2b9e7a644dcafbe62d046bed0e1a937dad95596
Jogjavier <jog20ortiz@gmail.com>
2024-12-11 23:51:29 -0600

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::resource('bombas', BombaController::class)->except(['show']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/bombas/search', [BombaController::class, 'search'])->name('bombas.search');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
